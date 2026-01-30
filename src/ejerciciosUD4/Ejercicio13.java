ed6ece2492f33c6e23fb31ee6f2f66805b813a7f
alberto <barreiravalin@gmail.com>
2026-01-30 10:42:47 +0100

    Ejercicio 13

--- src/ejerciciosUD4/Ejercicio13.java
+++ src/ejerciciosUD4/Ejercicio13.java
@@ -1,5 +1,6 @@
 package ejerciciosUD4;
 
+import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class Ejercicio13 {
@@ -56,15 +57,50 @@ public class Ejercicio13 {
         return v;
     }
     
+    static boolean esta(int[] v, int n){
+        for (int i = 0; i < v.length && v[i] <= n; i++) 
+            if (v[i] == n)
+                return true;
+        return false;
+    }
+    
+    static byte getAciertos(int[] ganador, int[] boleto){
+        // 4 6 8 10 16 20
+        // 2 3 4 6 9 10
+        byte contador = 0;
+        
+        for (int i = 0; i < boleto.length; i++) 
+            if (esta(ganador, boleto[i]))
+                contador++;
+        
+        return contador;
+    }
+    
     public static void main(String[] args) {
+        final int MAX = 100_000_000;
+        
         int[] ganador = generarBoleto();
+        // Ordenamos el vector ganador para acelerar las búsquedas
+        Arrays.sort(ganador);
+        
         int[] boleto;
         int[] aciertos = new int[7];
         
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < MAX; i++) {
             boleto = generarBoleto();
             aciertos[getAciertos(ganador, boleto)]++;
-                
+        }
+        
+        NumberFormat nf = NumberFormat.getInstance();
+        NumberFormat nfPorcentaje = NumberFormat.getPercentInstance();
+        nfPorcentaje.setMinimumFractionDigits(5);
+        
+        for (int i = 0; i < aciertos.length; i++) {
+            System.out.println(i + ": " + 
+                               nf.format(aciertos[i]) +
+                               " " +
+                               nfPorcentaje.format((double)aciertos[i]/MAX) 
+            );
         }
     }
 }
