7fbefb48160f950bdcf08d50f31b23d1ff7dc259
alberto <barreiravalin@gmail.com>
2026-03-11 10:16:34 +0100

    Ejercicio 37

--- src/ejerciciosUD4/Ejercicio37.java
+++ src/ejerciciosUD4/Ejercicio37.java
@@ -21,6 +21,32 @@ public class Ejercicio37 {
             System.out.println(Arrays.toString(m[f]));
     }
     
+    static boolean estaDentro(int[][] imagen, int f, int c){
+        return f >= 0 && f < imagen.length &&
+               c >= 0 && c < imagen[0].length;
+    }
+    
+    static int suavizar(int[][] imagen, int f, int c){
+        int[] df = {0, -1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dc = {0, -1, 0, 1, -1, 1, -1, 0, 1};
+        
+        int nf;
+        int nc;
+        int suma = 0;
+        int contador = 0;
+        
+        for (int i = 0; i < df.length; i++) {
+            nf = f + df[i];
+            nc = c + dc[i];
+            if (estaDentro(imagen, nf, nc)){
+                suma += imagen[nf][nc];
+                contador++;
+            }
+        }
+        
+        return suma / contador;
+    }
+    
     static int[][] suavizar(int[][] imagen){
         int[][] resultado = new int[imagen.length][imagen[0].length];
         
